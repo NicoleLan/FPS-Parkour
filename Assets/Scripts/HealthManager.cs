52c7e7dc44a33e719d9a419aae74cad50bed4587
NicoleLan <167232993+NicoleLan@users.noreply.github.com>
2025-07-08 11:57:56 -0300

    Terminado 2 (me olvide el respawn)

--- Assets/Scripts/HealthManager.cs
+++ Assets/Scripts/HealthManager.cs
@@ -1,19 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HealthManager : MonoBehaviour
 {
     public float playerHealth;
     public UImanager uiManager;
 
+
     public bool UpDateHealth(float amount)
     {
 
-        if (playerHealth + amount < 0)
+        if (playerHealth + amount <= 0)
         {
             //Impedir que se pueda hacer la operación
+            Debug.Log("Respawn");
+            SceneManager.LoadScene("FPS Parkour");
             return false;
+           
         }
         else
         {
@@ -27,6 +32,7 @@ public class HealthManager : MonoBehaviour
     private void Start()
     {
         uiManager.UpDateHealthUI(playerHealth.ToString());
+       
 
     }
 }
